bc8a5ffb522adb3778c7eb4b57fb624f5b6e354d
Alex Chichester <alexchi1114@gmail.com>
2021-08-20 20:46:50 -0400

    removing released games from list after two weeks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,12 +6,20 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Franchise;
 use App\Models\Game;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $upcoming_games = Game::Where('is_upcoming', 1)->orderBy(DB::raw('ISNULL(release_date), release_date, ISNULL(release_date_tentative), display_order'), 'ASC')->get();
+        foreach($upcoming_games as $game) {
+            if($game->release_date != null && $game->release_date->addDays(14)->lt(Carbon::now())) {
+                $game->is_upcoming = 0;
+                $game->save();
+            }
+        }
+        
         $franchises_to_watch = Franchise::getFranchisesToWatch()->take(5);
 
         return view('home.index', [
